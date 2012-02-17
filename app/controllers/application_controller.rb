761b59f990c5f605e56909a0bb030f938d6a760d
Johan Pretorius <johanp@strate.co.za>
2012-02-17 20:43:20 +0200

    Improved error handling logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  rescue_from Exception, :with => :render_error
+  rescue_from Exception, :with => :render_error     #Catch the unhandled exception
   
-  private
+  private                                           #Hide our error handling mechanism from the public
   
-  def render_error(exception)
-    redirect_to :action => "serve500", :controller => "error"
+  def render_error                                  #Define a method to handle the exception
+    redirect_to :action => "serve500", :controller => "error"    #Redirect to the 500 error controller/action
   end
 
 end
--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -1,11 +1,5 @@
 class ErrorController < ApplicationController
   def serve404
-    @not_found_path = params[:not_found]
-		
-	#if (@not_found_path = "exception")
-	#	redirect_to :action => "serve500"
-	redirect_to :action => "serve500" if /Error/ =~ @not_found_path
-	#end
   end
 
   def serve500
